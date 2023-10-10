afebf6b3b4fd2a6d01efe709bc6b04d3b9caa489
Stanislav Studzinskyi <stanislavs@wix.com>
2023-10-10 20:52:00 +0200

    20. Valid Parentheses - simple & basic one

--- src/main/scala/Stack.scala
+++ src/main/scala/Stack.scala
@@ -3,14 +3,28 @@ import scala.collection.mutable
 object Stack extends App {
   // 20. Valid Parentheses
   def isValid(s: String): Boolean = {
-    val stack = mutable.Stack
-    val chars = s.toCharArray
-    stack fill chars
+    if (s.isEmpty) return true
+    else if (s.length == 1) return false
 
-    false
+    val stack = mutable.Stack[Char](s(0))
+
+    for (c <- s.substring(1)) {
+      if (c == '{' || c == '(' || c == '[')
+        stack.push(c)
+      else {
+        val maybeHead = stack.headOption
+
+        if ((c == '}' && maybeHead.contains('{')) || (c == ')' && maybeHead.contains('(')) || (c == ']' && maybeHead.contains('['))) {
+          stack.pop()
+        } else {
+          return false
+        }
+      }
+    }
+
+    stack.isEmpty
   }
-
-  println(isValid("()[]{}"))
+//  println(isValid("(])"))
 
 
 }
