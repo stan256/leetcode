246767a6c72b4442628a319f671b799b6e454985
Stanislav Studzinskyi <stanislavs@wix.com>
2023-10-12 20:52:54 +0200

    155. Min Stack

--- src/main/scala/Stack.scala
+++ src/main/scala/Stack.scala
@@ -3,6 +3,8 @@ import scala.collection.mutable
 object Stack extends App {
   // 20. Valid Parentheses
   def isValid(s: String): Boolean = {
+    import scala.collection.mutable
+
     if (s.isEmpty) return true
     else if (s.length == 1) return false
 
@@ -25,6 +27,57 @@ object Stack extends App {
     stack.isEmpty
   }
 //  println(isValid("(])"))
+
+  // 155. Min Stack
+  class MinStack() {
+    import scala.collection.mutable
+
+    var stack = mutable.Stack[(Int, Int)]()
+    var min = 0
+
+    def push(i: Int): Unit = {
+      if (stack.isEmpty) {
+        min = i
+      }
+
+      if (i < min) {
+        min = i
+      }
+
+      stack = stack.push((i, min))
+    }
+
+    def pop(): Unit = {
+      val a = stack.pop()
+
+      if (a._2 == min) {
+        if (stack.isEmpty) {
+          min = 0
+        } else {
+          min = stack.head._2
+        }
+      }
+    }
+
+    def top(): Int = stack.head._1
+
+    def getMin(): Int = stack.head._2
+  }
+
+//  println {
+//    val x = new MinStack()
+//    x.push(-2)
+//    x.push(0)
+//    x.push(-3)
+//    x.getMin()
+//    x.pop()
+//    x.top()
+//    x.getMin()
+//    x.pop()
+//    x.pop()
+//  }
+
+
 
 
 }
