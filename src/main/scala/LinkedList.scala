3f42ec0afc47233aff06a7bd37994f308b1e3b21
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2023-11-01 19:42:38 +0100

    206. Reverse Linked List - my stupid solution

--- src/main/scala/LinkedList.scala
+++ src/main/scala/LinkedList.scala
@@ -1,6 +1,39 @@
 
 object LinkedList extends App {
 
+  // 206. Reverse Linked List
+  case class ListNode(_x: Int = 0, _next: ListNode = null) {
+    var next: ListNode = _next
+    var x: Int = _x
+  }
+  def reverseList(head: ListNode): ListNode = {
+    val stack = scala.collection.mutable.Stack.empty[Int]
+    var next = head
+    while (next != null) {
+      stack.push(next.x)
+      next = next.next
+    }
+
+    var start: ListNode = null
+    var prev: ListNode = null
+    while (stack.nonEmpty) {
+      if (start == null) {
+        start = ListNode(stack.pop())
+      } else {
+        if (prev == null) {
+          start.next = ListNode(stack.pop())
+          prev = start.next
+        } else {
+          prev.next = ListNode(stack.pop())
+          prev = prev.next
+        }
+      }
+    }
+    start
+  }
+
+  private val node: ListNode = reverseList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+  println(node)
 
 
 
