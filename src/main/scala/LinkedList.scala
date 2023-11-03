e9d2416506ecdf0a6346cd70692183c9695c19e2
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2023-11-03 22:47:05 +0100

    21. Merge Two Sorted Lists - intermediate commit, not solved

--- src/main/scala/LinkedList.scala
+++ src/main/scala/LinkedList.scala
@@ -44,9 +44,39 @@ object LinkedList extends App {
 
     reverse
   }
-  private val node: ListNode = reverseList2(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
-  println(node)
+//  private val node: ListNode = reverseList2(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+//  println(node)
 
+  // 21. Merge Two Sorted Lists
+  def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
+
+    val (first, second) = if (list1.x < list2.x) (list1, list2) else (list1, list2)
+
+    var k = first
+    var iterator1 = first.next
+    var iterator2 = second
+
+    while (iterator1 != null || iterator2 != null) {
+      (iterator1, iterator2) match {
+        case (a, b) if ((a != null && b != null ) && a.x < b.x) || b == null  => {
+          iterator1 = a.next
+          k.next = a
+          k.next.next = iterator2
+        }
+        case (a, b) if ((a != null && b != null ) && a.x >= b.x) || a == null => {
+          iterator2 = iterator2.next
+          k.next = b
+          k.next.next = iterator1
+        }
+        case _ => throw new RuntimeException()
+      }
+    }
+
+
+    first
+  }
+  val node: ListNode = mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))), ListNode(1, ListNode(3, ListNode(4))))
+  println(node)
 
 
 }
