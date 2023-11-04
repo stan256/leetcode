9cbc6e59d7443690605260b1d53c48f36d6e5b55
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2023-11-04 22:06:39 +0100

    143. Reorder List - as always - my dummy solution first

--- src/main/scala/LinkedList.scala
+++ src/main/scala/LinkedList.scala
@@ -6,6 +6,7 @@ object LinkedList extends App {
     var next: ListNode = _next
     var x: Int = _x
   }
+
   def reverseList(head: ListNode): ListNode = {
     val stack = scala.collection.mutable.Stack.empty[Int]
     var next = head
@@ -44,8 +45,8 @@ object LinkedList extends App {
 
     reverse
   }
-//  private val node: ListNode = reverseList2(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
-//  println(node)
+  //  private val node: ListNode = reverseList2(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+  //  println(node)
 
   // 21. Merge Two Sorted Lists
   def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
@@ -63,12 +64,12 @@ object LinkedList extends App {
 
     while (iterator1 != null || iterator2 != null) {
       (iterator1, iterator2) match {
-        case (a, b) if ((a != null && b != null ) && a.x < b.x) || b == null  =>
+        case (a, b) if ((a != null && b != null) && a.x < b.x) || b == null =>
           iterator1 = a.next
           k.next = a
           k.next.next = iterator2
           k = k.next
-        case (a, b) if ((a != null && b != null ) && a.x >= b.x) || a == null =>
+        case (a, b) if ((a != null && b != null) && a.x >= b.x) || a == null =>
           iterator2 = b.next
           k.next = b
           k.next.next = iterator1
@@ -80,10 +81,44 @@ object LinkedList extends App {
 
     first
   }
-//  val node: ListNode = mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))), ListNode(1, ListNode(3, ListNode(4))))
-//  val node: ListNode = mergeTwoLists(ListNode(2), ListNode(1))
-//  println(node)
+  //  val node: ListNode = mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))), ListNode(1, ListNode(3, ListNode(4))))
+  //  val node: ListNode = mergeTwoLists(ListNode(2), ListNode(1))
+  //  println(node)
 
+  // 143. Reorder List
+  def reorderList(head: ListNode): Unit = {
+    var iterator = head
+    var stack = scala.collection.mutable.Stack.empty[Int]
+    while (iterator != null) {
+      stack.push(iterator.x)
+      iterator = iterator.next
+    }
+    val length = stack.length
+    stack = stack.take(length / 2)
 
+    iterator = head
+    var change = true
+    while (stack.nonEmpty) {
+      if (change) {
+        iterator.next = ListNode(stack.pop(), iterator.next)
+        change = false
+      } else
+        change = true
+      iterator = iterator.next
+    }
+
+    if (length > 1) {
+      if (length % 2 == 0)
+        iterator.next = null
+      else
+        iterator.next.next = null
+    }
+  }
+
+  //    private val node: ListNode = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+  private val node: ListNode = ListNode(1)
+  //  private val node: ListNode = ListNode(1, ListNode(2))
+  //  private val node: ListNode = ListNode(1, ListNode(2, ListNode(3)))
+  reorderList(node)
 
 }
