c2167552fb5e6f5822d835d0a0dff67a634cd73f
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2023-12-05 20:42:36 +0100

    235. Lowest Common Ancestor of a Binary Search Tree

--- src/main/scala/BinaryTree.scala
+++ src/main/scala/BinaryTree.scala
@@ -1,5 +1,4 @@
 
-
 object BinaryTree extends App {
   class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
     var value: Int = _value
@@ -21,9 +20,29 @@ object BinaryTree extends App {
         depth
       else
         Math.max(rec(node.left, depth + 1), rec(node.right, depth + 1))
+
     rec(root, 0)
   }
 
+  // 235. Lowest Common Ancestor of a Binary Search Tree
+  def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): TreeNode = {
+    def find(node: TreeNode): TreeNode =
+      (p, q) match {
+        case (a, b) if a.value < node.value && b.value < node.value => find(node.left)
+        case (a, b) if a.value > node.value && b.value > node.value => find(node.right)
+        case _ => node
+      }
+
+    find(root)
+  }
+
+//  val a = new TreeNode(2)
+//  val bbigger1 = new TreeNode(7)
+//  val bsmaller1 = new TreeNode(5)
+//  val b = new TreeNode(6, bsmaller1, bbigger1)
+//  val root = new TreeNode(4, a, b)
+//
+//  println(lowestCommonAncestor(root, bbigger1, bsmaller1).value)
 
 
 }
