23ec757cda2de6025e7db0351864f043ee0089de
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2023-12-22 21:17:22 +0100

    1426. Counting Elements

--- src/main/scala/Hashing.scala
+++ src/main/scala/Hashing.scala
@@ -25,4 +25,19 @@ object Hashing extends App {
   //  println(countElements(Array(1,2,3)))
   //  println(countElements(Array(1,3, 5,5)))
 
+  // 1426. Counting Elements
+  def findWinners(matches: Array[Array[Int]]): List[List[Int]] = {
+    val map = scala.collection.mutable.HashMap.empty[Int, Int]
+    matches.foreach(x => {
+      val looser = x(1)
+      val winner = x(0)
+      map.put(looser, map.getOrElse(looser, 0) + 1)
+      if (!map.contains(winner)) map.put(winner, 0)
+    })
+    val haveNotLostAny = map.filter(x => x._2 == 0).keys.toList
+    val lostOne = map.filter(x => x._2 == 1).keys.toList
+    haveNotLostAny.sorted :: lostOne.sorted :: Nil
+  }
+
+  // println(findWinners(Array(Array(1, 3), Array(2, 3), Array(3, 6), Array(5, 6), Array(5, 7), Array(4, 5), Array(4, 8), Array(4, 9), Array(10, 4), Array(10, 9))))
 }
