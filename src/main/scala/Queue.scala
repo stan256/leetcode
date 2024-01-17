0a6e0fd665344b8d55b6ea94935b5a4577a50edf
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2024-01-17 19:27:32 +0100

    346. Moving Average from Data Stream

--- src/main/scala/Queue.scala
+++ src/main/scala/Queue.scala
@@ -9,4 +9,18 @@ object Queue extends App {
     list.size
   }
 
+  // 346. Moving Average from Data Stream
+  class MovingAverage(_size: Int) {
+    private var list = List.empty[Int]
+    private var currentSum: Double = 0
+    def next(value: Int): Double = {
+      list = list :+ value
+      currentSum += value
+      if (list.size > _size) {
+        currentSum -= list.head
+        list = list.drop(1)
+      }
+      currentSum / Math.min(list.size, _size)
+    }
+  }
 }
