4c6a44a82350e4429d9ed521eedc62f24e299d4f
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2024-02-22 19:17:00 +0100

    2208. Minimum Operations to Halve Array Sum

--- src/main/scala/Heaps.scala
+++ src/main/scala/Heaps.scala
@@ -2,7 +2,7 @@
 object Heaps extends App {
   // 1046. Last Stone Weight
   def lastStoneWeight(stones: Array[Int]): Int = {
-    val pq = collection.mutable.PriorityQueue(stones:_*)
+    val pq = collection.mutable.PriorityQueue(stones: _*)
 
     while (pq.size > 1) {
       val y = pq.dequeue
@@ -14,5 +14,23 @@ object Heaps extends App {
 
     pq.lastOption.getOrElse(0)
   }
+
+  // 2208. Minimum Operations to Halve Array Sum
+  def halveArray(nums: Array[Int]): Int = {
+    val sum = nums.map(_.toDouble).sum
+    val target: Double = sum / 2d
+    var currentSum: Double = sum
+    val queue = collection.mutable.PriorityQueue[Double](nums.map(_.toDouble):_*)
+
+    var ans = 0
+    while (currentSum >= target) {
+      val x = queue.dequeue
+      queue.enqueue(x / 2)
+      currentSum -= x / 2
+      ans += 1
+    }
+    ans
+  }
+
 
 }
