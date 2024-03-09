301a89da0d34bfd037ccbce26f585b78e6052cf1
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2024-03-09 09:11:23 +0100

    2294. Partition Array Such That Maximum Difference Is K

--- src/main/scala/GreedyAlgorithms.scala
+++ src/main/scala/GreedyAlgorithms.scala
@@ -1,6 +1,28 @@
+
+
 object GreedyAlgorithms extends App {
   // 2126. Destroying Asteroids
   def asteroidsDestroyed(mass: Int, asteroids: Array[Int]): Boolean = {
     asteroids.sorted.foldLeft[Long](mass) { (mass, x) => if (mass < x) mass - x else mass + x } > 0
+  }
+
+  // 2294. Partition Array Such That Maximum Difference Is K
+  def partitionArray(nums: Array[Int], k: Int): Int = {
+    import scala.collection.mutable
+
+    nums.sorted.foldLeft(mutable.Stack[mutable.ArrayDeque[Int]]()) { (stack, num) =>
+      if (stack.isEmpty) {
+        stack.push(mutable.ArrayDeque(num))
+      } else {
+        val queue = stack.head
+        val value = queue.head
+        if (Math.abs(num - value) <= k) {
+          queue.addOne(num)
+        } else {
+          stack.push(mutable.ArrayDeque(num))
+        }
+      }
+      stack
+    }.size
   }
 }
