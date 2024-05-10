2b356ecd4a41ea9659cd4d3d4c0f88fc652aed08
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2024-05-10 18:29:22 +0200

    78. Subsets

--- src/main/scala/Backtracking.scala
+++ src/main/scala/Backtracking.scala
@@ -17,4 +17,25 @@ object Backtracking extends App {
     backtracking(List.empty)
     answer
   }
+
+  // 78. Subsets
+  def subsets(nums: Array[Int]): List[List[Int]] = {
+    var answer = List.empty[List[Int]]
+
+    def backtracking(curr: List[Int]): Unit = {
+      if (!answer.contains(curr)) answer = answer :+ curr
+
+      for (num <- nums) {
+        if (!curr.contains(num))
+          backtracking(curr.appended(num))
+      }
+    }
+
+    backtracking(List.empty)
+    answer
+  }
+
+  println(subsets(Array(1, 2, 3)))
+  println(subsets(Array(4,1,0)))
+
 }
