237143a0ed35343f054a713cd773501d82f18c8c
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2024-05-10 18:02:44 +0200

    46. Permutations - better

--- src/main/scala/Backtracking.scala
+++ src/main/scala/Backtracking.scala
@@ -1,24 +1,31 @@
 
 object Backtracking extends App {
+
   // 46. Permutations
-  def permute(nums: Array[Int]): List[List[Int]] =
-    helper(i = 0, arr = nums, permutations = Nil)
+  def permute(nums: Array[Int]): List[List[Int]] = {
+    import collection.mutable
 
-  def helper(i: Int, arr: Array[Int], permutations: List[List[Int]]): List[List[Int]] =
-    if (i == arr.length)
-      permutations.appended(arr.toList)
-    else
-      (i until arr.length).foldLeft(List.empty[List[Int]]) {
-        case (acc, j) =>
-          swap(arr, i, j)
-          val perms = helper(i + 1, arr, permutations)
-          swap(arr, i, j)
-          perms ::: acc
+    var answer = Seq.empty[mutable.Seq[Int]]
+    var curr = mutable.Buffer.empty[Int]
+
+    def backtracking(): Unit = {
+      if (curr.length == nums.length) {
+        answer = answer :+ curr.clone()
+      } else {
+        for (num <- nums) {
+          if (!curr.contains(num)) {
+            val i = curr.length
+            curr = curr.appended(num)
+            backtracking()
+            curr.remove(i)
+          }
+        }
       }
+    }
 
-  def swap(arr: Array[Int], i: Int, j: Int): Unit = {
-    val tmp = arr(i)
-    arr(i) = arr(j)
-    arr(j) = tmp
+    backtracking()
+    answer.map(_.toList).toList
   }
+
+  println(permute(Array(1,2,3)))
 }
