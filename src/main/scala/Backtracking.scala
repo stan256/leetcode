169b76db81a191f97cea5cdbf17641555a0ed538
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2024-05-11 17:17:49 +0200

    78. Subsets

--- src/main/scala/Backtracking.scala
+++ src/main/scala/Backtracking.scala
@@ -22,20 +22,21 @@ object Backtracking extends App {
   def subsets(nums: Array[Int]): List[List[Int]] = {
     var answer = List.empty[List[Int]]
 
-    def backtracking(curr: List[Int]): Unit = {
-      if (!answer.contains(curr)) answer = answer :+ curr
+    def backtracking(curr: List[Int], i: Int): Unit = {
+      if (!answer.contains(curr))
+        answer = answer :+ curr
 
-      for (num <- nums) {
-        if (!curr.contains(num))
-          backtracking(curr.appended(num))
+      for (x <- nums.drop(i).zipWithIndex) {
+        if (!curr.contains(x._1))
+          backtracking(curr.appended(x._1), i + x._2)
       }
     }
 
-    backtracking(List.empty)
+    backtracking(List.empty, 0)
     answer
   }
 
-  println(subsets(Array(1, 2, 3)))
-  println(subsets(Array(4,1,0)))
+  // println(subsets(Array(1, 2, 3)))
+  // println(subsets(Array(4,1,0)))
 
 }
