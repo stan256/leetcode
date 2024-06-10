3cf06a9c22d17419f47f530c56dc770169e192aa
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2024-06-10 18:07:46 +0200

    17. Letter Combinations of a Phone Number

--- src/main/scala/Backtracking.scala
+++ src/main/scala/Backtracking.scala
@@ -83,4 +83,36 @@ object Backtracking extends App {
 
     answer.map(_.toList).toList
   }
+
+  // 17. Letter Combinations of a Phone Number
+  def letterCombinations(digits: String): List[String] = {
+    if (digits.isEmpty)
+      return List()
+
+    val keys = Map(
+      "2" -> "abc".toCharArray,
+      "3" -> "def".toCharArray,
+      "4" -> "ghi".toCharArray,
+      "5" -> "jkl".toCharArray,
+      "6" -> "mno".toCharArray,
+      "7" -> "pqrs".toCharArray,
+      "8" -> "tuv".toCharArray,
+      "9" -> "wxyz".toCharArray,
+    )
+
+    val list = scala.collection.mutable.ListBuffer[String]()
+
+    def backtracking(s: String, result: String): Unit = {
+      if (s.length == 1) keys(s).foreach(r => list += (result + r))
+      else keys(s.charAt(0).toString).foreach(ch => backtracking(s.substring(1), result + ch))
+    }
+
+    backtracking(digits, "")
+
+    list.toList
+  }
+
+  println(letterCombinations("23"))
+  println(letterCombinations(""))
+  println(letterCombinations("2"))
 }
