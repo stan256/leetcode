961e1448399e6ac29789c7cba58817871de372c6
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2024-07-28 11:05:15 -0400

    12. Integer to Roman - better more uniform solution

--- src/main/scala/Arrays.scala
+++ src/main/scala/Arrays.scala
@@ -656,4 +656,49 @@ object Arrays extends App {
 
     sb.toString
   }
+
+  // 12. Integer to Roman - better
+  def intToRoman_2(nums: Int): String = {
+    val str = nums.toString
+
+    val sb = new StringBuilder()
+
+    for (t <- str.zipWithIndex.map(x => (x._1.toString.toInt, x._2))) {
+      val order = str.length - t._2
+
+      def one(order: Int) =
+        order match {
+          case 4 => "M"
+          case 3 => "C"
+          case 2 => "X"
+          case 1 => "I"
+        }
+
+      def five(order: Int) =
+        order match {
+          case 3 => "D"
+          case 2 => "L"
+          case 1 => "V"
+        }
+
+      t._1 match {
+        case 0 =>
+        case x if 1 to 3 contains x =>
+          sb.append(one(order) * t._1)
+        case 4 =>
+          sb.append(one(order))
+          sb.append(five(order))
+        case 5 =>
+          sb.append(five(order))
+        case x if 6 to 8 contains x =>
+          sb.append(five(order))
+          sb.append(one(order) * (t._1 - 5))
+        case 9 =>
+          sb.append(one(order))
+          sb.append(one(order + 1))
+      }
+    }
+
+    sb.toString
+  }
 }
