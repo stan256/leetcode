0942b347f8a2f7162570b0385ce12e4ee07c9d35
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2025-02-12 18:59:58 -0500

    2043. Simple Bank System

--- src/main/scala/General.scala
+++ src/main/scala/General.scala
@@ -19,8 +19,39 @@ object General extends App {
     -1
   }
 
-  println(binarySearch(Array(1, 3, 4, 5, 8, 12, 14, 19, 25, 44, 55, 66, 67, 90, 119), 14))
-  println(binarySearch(Array(-1,0,3,5,9,12), 12))
+  // 2043. Simple Bank System
+  class Bank(balances: Array[Long]) {
+    def addMoney(n: Int, money: Long): Unit = {
+      balances(n) += money
+    }
 
+    def subtractMoney(n: Int, money: Long): Unit = {
+      balances(n) -= money
+    }
+
+    def accountExists(account: Int): Boolean = account <= balances.length
+
+    def transfer(account1: Int, account2: Int, money: Long): Boolean = {
+      if (accountExists(account1) && accountExists(account2) && balances(account1 - 1) >= money) {
+        addMoney(account2 - 1, money)
+        subtractMoney(account1 - 1, money)
+        true
+      } else false
+    }
+
+    def deposit(account: Int, money: Long): Boolean = {
+      if (accountExists(account)) {
+        addMoney(account - 1, money)
+        true
+      } else false
+    }
+
+    def withdraw(account: Int, money: Long): Boolean = {
+      if (accountExists(account) && balances(account - 1) >= money) {
+        subtractMoney(account - 1, money)
+        true
+      } else false
+    }
+  }
 
 }
