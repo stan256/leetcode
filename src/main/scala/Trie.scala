ebd05f4461b3ddf8827e23747862e9bfe43949cf
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2025-03-25 11:56:53 -0400

    208. Implement Trie

--- src/main/scala/Trie.scala
+++ src/main/scala/Trie.scala
@@ -31,4 +31,42 @@ object Trie extends App {
     }
   }
 
+  // // 208. Implement Trie (Prefix Tree)
+  class Trie_2() {
+    val parent = TrieNode()
+
+    def insert(word: String): Unit = {
+      var level = parent
+      for (c <- word) {
+        if (level.arr(c) == null) level.arr(c) = TrieNode()
+        level = level.arr(c)
+      }
+      level.end = true
+    }
+
+    def search(word: String): Boolean = {
+      var level = parent
+      val result = word.find(c => {
+        if (level.arr(c) != null) {
+          level = level.arr(c)
+          false
+        } else true
+      })
+      result.isEmpty && level.end
+    }
+
+    def startsWith(prefix: String): Boolean = {
+      var level = parent
+      val result = prefix.find(c => {
+        if (level.arr(c) != null) {
+          level = level.arr(c)
+          false
+        } else true
+      })
+      result.isEmpty
+    }
+
+    case class TrieNode(val arr: Array[TrieNode] = Array.fill[TrieNode]('a' + 26)(null), var end: Boolean = false)
+  }
+
 }
